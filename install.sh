258a9d8beb7fdcfbfcbe182af26595d4fff51edb
Ugo Jardonnet <u.jardonnet@criteo.com>
2013-09-29 00:07:35 +0200

    Fix line ending

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -s ${DIR}/zshrc ~/.zshrc
-ln -s ${DIR}/gitconfig ~/.gitconfigc
+ln -s ${DIR}/gitconfig ~/.gitconfig
 ln -s ${DIR}/emacsd ~/.emacsd
