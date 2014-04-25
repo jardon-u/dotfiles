dd0fa9c932ad99faf1a87ed19b4faf1e446c50bc
Ugo Jardonnet <u.jardonnet@criteo.com>
2014-04-25 16:08:49 +0200

    Improve install script

--- install.sh
+++ install.sh
@@ -2,6 +2,14 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -s ${DIR}/zshrc ~/.zshrc
-ln -s ${DIR}/gitconfig ~/.gitconfig
-ln -s ${DIR}/emacs.d ~/.emacs.d
+ln -sinv ${DIR}/zshrc ~/.zshrc
+ln -sinv ${DIR}/gitconfig ~/.gitconfig
+ln -sinv ${DIR}/emacs.d ~/.emacs.d
+
+dpkg --get-selections emacs
+dpkg --get-selections emacs-goodies-el
+dpkg --get-selections ipython
+dpkg --get-selections pymacs
+dpkg --get-selections pylint
+dpkg --get-selections pyflakes
+dpkg --get-selections zsh
