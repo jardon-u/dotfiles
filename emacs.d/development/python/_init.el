c91d4f84e76aa17fe04fadd43eae246886611d3b
Ugo Jardonnet <ugodotjardonnetatgmail>
2014-11-11 15:39:54 +0100

    Improve python mode

--- emacs.d/development/python/_init.el
+++ emacs.d/development/python/_init.el
@@ -17,19 +17,9 @@
 (autoload 'pymacs-call "pymacs")
 
 ;(require 'pycomplete)
+(add-hook 'python-mode-hook 'flycheck-mode)
+(setq flycheck-flake8-maximum-line-length 120)
 
-(when (load "flymake" t)
-  (defun flymake-pylint-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list "epylint" (list local-file))))
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pylint-init)))
-
-(add-hook 'python-mode-hook 'flymake-mode)
 ;(add-hook 'python-mode-hook 'highlight-indentation-current-column-mode)
 (add-hook 'python-mode-hook 'highlight-indentation-mode)
 ;(set-face-background 'highlight-indentation-face "#e3e3d3")
