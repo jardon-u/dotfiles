7558861411ad72f7c63f33f71f738ea55afa8767
Ugo Jardonnet <ugodotjardonnetatgmail>
2014-12-05 12:08:25 +0100

    package info

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,15 +1,16 @@
-(setq CONFIGURATION-PATH (expand-file-name "~/.emacs.d"))
-(setq load-path (cons CONFIGURATION-PATH load-path))
+;(setq CONFIGURATION-PATH (expand-file-name "~/.emacs.d"))
+;(setq load-path (cons CONFIGURATION-PATH load-path))
 
 ;; Development
 
 (require 'package)
 (package-initialize)
-(add-to-list 'package-archives
-  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(setq package-archives
+      '(("ELPA" . "http://tromey.com/elpa/")
+        ("gnu" . "http://elpa.gnu.org/packages/")
+        ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 (load "~/.emacs.d/development/_init")
 (load "~/.emacs.d/display/_init")
 (load "~/.emacs.d/editing/_init")
 (load "~/.emacs.d/navigation/_init")
-
