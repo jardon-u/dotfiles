e84398dd3aaae8f8f3df3e9c832593992810c636
Ugo Jardonnet <ugodotjardonnetatgmail>
2014-12-05 12:07:53 +0100

    C++ irony for emacs

--- emacs.d/development/c/_init.el
+++ emacs.d/development/c/_init.el
@@ -108,3 +108,20 @@
  '(flycheck-googlelint-linelength "120")
  '(flycheck-googlelint-root "placemeter-suite.pmcv"))
 
+
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+
+;; replace the `completion-at-point' and `complete-symbol' bindings in
+;; irony-mode's buffers by irony-mode's asynchronous function
+(defun my-irony-mode-hook ()
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async))
+(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+
+;; Only needed on Windows
+(when (eq system-type 'windows-nt)
+  (setq w32-pipe-read-delay 0))
