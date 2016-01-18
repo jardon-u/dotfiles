2ea4e0d9675dc71d73dece16d45ad2a7c3c3f330
Ugo Jardonnet <ugodotjardonnetatgmail>
2016-01-18 12:13:13 +0100

    Clean python mode

--- emacs.d/development/python/_init.el
+++ emacs.d/development/python/_init.el
@@ -1,21 +1,6 @@
-;(setq ipython-command "/usr/bin/ipython")
-;(require 'ipython)
-
-;; python-mode settings
-(setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
-(setq interpreter-mode-alist(cons '("python" . python-mode)
-                             interpreter-mode-alist))
-;; path to the python interpreter, e.g.: ~rw/python27/bin/python2.7
-(setq py-python-command "python")
-(autoload 'python "python" "Python editing mode." t)
-
-;; pymacs settings
-(setq pymacs-python-command py-python-command)
-(autoload 'pymacs-load "pymacs" nil t)
-(autoload 'pymacs-eval "pymacs" nil t)
-(autoload 'pymacs-apply "pymacs")
-(autoload 'pymacs-call "pymacs")
-
-;(require 'pycomplete)
 (add-hook 'python-mode-hook 'flycheck-mode)
 (add-hook 'python-mode-hook 'highlight-indentation-mode)
+
+(defun my/python-mode-hook ()
+  (add-to-list 'company-backends 'company-jedi))
+(add-hook 'python-mode-hook 'my/python-mode-hook)
