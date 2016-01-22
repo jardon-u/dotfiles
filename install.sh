3d9116655b8d5d16037fc60e6b9b3c7a09afb73e
Ugo Jardonnet <ugodotjardonnetatgmail>
2016-01-22 11:34:49 +0100

    Improve install script

--- install.sh
+++ install.sh
@@ -6,10 +6,29 @@ ln -sinv ${DIR}/zshrc ~/.zshrc
 ln -sinv ${DIR}/gitconfig ~/.gitconfig
 ln -sinv ${DIR}/emacs.d ~/.emacs.d
 
-dpkg --get-selections emacs
-dpkg --get-selections emacs-goodies-el
-dpkg --get-selections ipython
-dpkg --get-selections pymacs
-dpkg --get-selections pylint
-dpkg --get-selections pyflakes
-dpkg --get-selections zsh
+# install minimal tools to survive
+sudo apt-get install emacs24 git zsh cmake ipython
+# install python stuff (needed by emacs conf)
+sudo apt-get install python-virtualenv pylint
+# install c++ stuff (needed by emacs conf)
+sudo apt-get install clang-3.4 libclang-3.4-dev
+
+
+# Zsh
+read -p "Do you want to set zsh as your default shell ? (y) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    sudo chsh $USER -s $(which zsh)
+fi
+
+# emacs configuration
+pushd ~/.emacs.d
+bash ./install-cask.sh
+echo "You may need to add export PATH=\"$PATH:$HOME/.cask/bin/\" to you .zshrc or .bashrc"
+cask install
+popd
+
+echo "*** WARNING *** Now run the following command inside emacs":
+echo " M-x irony-install-server "
+echo " M-x jedi:install-server "
