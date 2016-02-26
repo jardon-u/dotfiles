81c9464c93b105ea39574ba186e6b06edaa7ea70
Ugo Jardonnet <ugodotjardonnetatgmail>
2016-02-26 17:03:47 +0100

    Switch to Python anaconda-mode and add key bindings

--- emacs.d/development/python/_init.el
+++ emacs.d/development/python/_init.el
@@ -1,13 +1,26 @@
-(add-hook 'python-mode-hook 'flycheck-mode)
-(add-hook 'python-mode-hook 'highlight-indentation-mode)
 
-(defun my/python-mode-hook ()
-  (add-to-list 'company-backends 'company-jedi))
-(add-hook 'python-mode-hook 'my/python-mode-hook)
+;; (defun my/python-mode-hook ()
+;;   (add-to-list 'company-backends 'company-jedi))
+;; (add-hook 'python-mode-hook 'my/python-mode-hook)
+;; (add-hook 'python-mode-hook 'eldoc-mode)
 
 ;; (eval-after-load "company"
 ;;  '(progn
 ;;    (add-to-list 'company-backends 'company-anaconda)))
 
-;(add-hook 'python-mode-hook 'anaconda-mode)
-;(add-hook 'python-mode-hook 'eldoc-mode)
+
+(add-hook 'python-mode-hook 'flycheck-mode)
+(add-hook 'python-mode-hook 'highlight-indentation-mode)
+
+(eval-after-load "company"
+ '(add-to-list 'company-backends 'company-anaconda))
+(add-hook 'python-mode-hook 'anaconda-mode)
+(add-hook 'python-mode-hook 'anaconda-eldoc-mode)
+
+(defvar anaconda-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-j") 'anaconda-mode-find-definitions)
+    (define-key map (kbd "C-d") 'anaconda-mode-find-assignments)
+    (define-key map (kbd "C-f") 'anaconda-mode-find-references)
+    map)
+  "Keymap for `anaconda-mode'.")
