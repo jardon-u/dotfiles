1fe55c2af6bd80e629db40ad96fd9147064b8bd9
Ugo Jardonnet <ugodotjardonnetatgmail>
2020-02-05 11:15:20 +0100

    Update to color-theme-modern

--- emacs.d/display/_init.el
+++ emacs.d/display/_init.el
@@ -1,12 +1,15 @@
 (load "~/.emacs.d/display/visual.el")
 (load "~/.emacs.d/display/text-display.el")
 
-(require 'color-theme)
-(color-theme-initialize)
-(color-theme-clarity)
+(load-theme 'clarity t t)
+(enable-theme 'clarity)
 
 ;; zoom in/out
 (global-set-key (kbd "<C-mouse-5>") 'text-scale-decrease)
 (global-set-key (kbd "<C-mouse-4>") 'text-scale-increase)
 (global-set-key (kbd "C--") 'text-scale-decrease)
 (global-set-key (kbd "C-+") 'text-scale-increase)
+
+
+(custom-set-variables              ; Sets show-trailing-whitespace as expected
+ '(show-trailing-whitespace t))
